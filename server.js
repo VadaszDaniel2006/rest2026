40c20f45f6f0203fe06bea5c002973a7297185d6
HermannZsombor <zsombor.vonyarc@gmail.com>
2025-09-24 10:30:19 +0200

    Post és Delete végpontok elkészítése

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.get('/hello', (req, res) => {
     res.send("Hello itt az Express webszerver!");
 })
 
-app.get('/api/courses', (req, res) => {
+app.get('/api.courses', (req, res) => {
     res.json(courses);
 })
 
 
 //Egyetlen kurzus adatainak lekérése url paraméter alapján
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api.courses/:id', (req, res) => {
     //Keresés a tömben ID(url paraméter) alapján
     const course = courses.find(c => c.id === parseInt(req.params.id));
     //A keresett elem nem található(404) státuszkód és hibaüzenet visszaadása
@@ -31,16 +31,30 @@ app.get('/api/courses/:id', (req, res) => {
     res.json(course); //A visszaadjuk a keresett kurzust
 })
 
-//POST végpont létrehozása kurzus adatok küldésére a szervernek
-app.post('/api/courses', (req, res) => {
+//POST végpont kurzus adatok küldésére 
+app.post('/api.courses', (req, res) => {
 //Új kurzus objektum létrehozása (Az ID automatikus növelése)
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
-course.push(course); //Az új kurzus objektum hozzáadása a courses tömbhöz
-res.json(req.body); //A kibővített courses adatainak a lekérése JSON formátumban
+    //A curse objektum hozzáadása a courses tömbhöz
+    courses.push(course); 
+res.status(200).json({message: "Új elem hozzáadva", data:req.body}); 
 })
+
+//Delete végpont kurzus adatok törlésére
+app.delete('/api.courses/:id',(req, res) => {
+    //Keresés a tömben ID(url paraméter) alapján
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //A keresett elem nem található(404) státuszkód és hibaüzenet visszaadása
+    if (!course) res.status(404).send('A megadott ID-val nem létezik kurzus!');
+    
+    const index = courses.indexOf(course);//A törölni kívánt elem indexének a meghatározása a courses tömbben
+    courses.splice(index, 1);//Az elem törlése a courses tömbből
+    res.json({message: "Sikeres adat törlés", data: req.body})//A törlés nyugtázása
+})
+
 
 
 //A webszerver elindítása
